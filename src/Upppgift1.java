4f7aed14c49411d8f80922cf1222c6a21fe787ee
Willex275 <willestrandberg14@gmail.com>
2019-08-26 10:42:14 +0200

    commit1

---
+++ src/Upppgift1.java
@@ -0,0 +1,17 @@
+
+public class Upppgift1 {
+
+	public static void main(String[] args) {
+
+		int[] siffror = new int[30];
+		for (int i = 0; i < siffror.length; i++) {
+			siffror[i] = i + 1;
+		}
+		int summa = 0;
+		for (int i = 0; i < siffror.length; i++) {
+			summa += siffror[i];
+		}
+		System.out.println(summa);
+	}
+
+}
