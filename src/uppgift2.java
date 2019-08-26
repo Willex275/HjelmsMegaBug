a63ed2f43709bca9bdb3d6735508a529b1cf43b6
Willex275 <willestrandberg14@gmail.com>
2019-08-26 10:49:22 +0200

    commit2

---
+++ src/uppgift2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class uppgift2 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in); 
+		
+		String ett = input.nextLine();
+		String tva = input.nextLine();
+		String tre = input.nextLine();
+		
+		System.out.println(tre);
+		System.out.println(tva);
+		System.out.println(ett);
+	}
+	
+
+}
