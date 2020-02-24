927b9f953c3c4e3f26f74fffb0b5aeb25b22e5be
Willex275 <willestrandberg14@gmail.com>
2020-02-24 10:06:44 +0100

    Klar med Uppgiften Webb-propro2

--- NodeIntro/StartNode.js
+++ NodeIntro/StartNode.js
@@ -7,8 +7,25 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '\\startnode.html') 
 })
 
-app.get('/Style.css', (req, res) => {
+app.get('/ass', (req, res) => {
   res.sendFile(__dirname + '\\Style.css')
  })
+
+ app.get('/hole', (req, res) => {
+  res.sendFile(__dirname + '\\RR.jpg')
+ })
+
+
+ app.use(express.json())
+app.use(express.urlencoded())
+
+ app.post('/form', (req, res) => {
+  const name = req.body.name
+  console.log(Name)
+  res.redirect('/RR.jpg')
+})
+ 
+
+
 
 app.listen(port, () => console.log(`Example app listening on port port!`))
